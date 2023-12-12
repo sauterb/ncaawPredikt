c5748dee9dd174635c00bf1e1188bdff200c0164
sauterb <61859576+sauterb@users.noreply.github.com>
2023-12-12 12:17:24 -0700

    Update NN_models.py

--- NN_models.py
+++ NN_models.py
@@ -1,5 +1,4 @@
 import numpy as np
-from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, InputLayer, SimpleRNN
 from sklearn.model_selection import train_test_split
