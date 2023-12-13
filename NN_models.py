7723e60b7883e52ca137957b5999c566d1f8ae7b
sauterb <61859576+sauterb@users.noreply.github.com>
2023-12-12 21:35:40 -0700

    Add files via upload

--- NN_models.py
+++ NN_models.py
@@ -1,9 +1,11 @@
+# CSCI 5922 Final Project
+# Bailey Sauter, Fall 2023
+
 import numpy as np
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, InputLayer, SimpleRNN
+from keras.layers import LSTM, Dense, Dropout, InputLayer, Lambda
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 
 # Function for plotting loss and accuracy across epochs
 def twoAxisEpochsPlot(accuracy, loss, num_epochs):
@@ -36,38 +38,42 @@ def RNN_model(X,y):
     # Split into training and testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=99)
 
-    scaler = StandardScaler()
-
     print("Running RNN Model")
 
+    # Build sequential Keras model
     ncaaw_rnn = Sequential([
         InputLayer(input_shape=(X.shape[1],X.shape[2])),
         LSTM(units=100, return_sequences=True),
         Dense(64, activation='relu'),
         Dropout(rate=0.25),
         Dense(16, activation='relu'),
-        # TODO: adjust sigmoid so predictions aren't so "certain"
         Dense(1, activation='sigmoid')
     ])
 
+    # Compile model
     ncaaw_rnn.compile(optimizer='adam',
                       loss='binary_crossentropy',
                       metrics=['accuracy'])
 
+    # Print model summary
     ncaaw_rnn.summary()
-    training = ncaaw_rnn.fit(X_train, y_train, batch_size=128, epochs=125, use_multiprocessing=True, verbose=True)
+
+    # Train model while tracking accuracy and loss metrics
+    training = ncaaw_rnn.fit(X_train, y_train, batch_size=256, epochs=100, use_multiprocessing=True, verbose=True)
     accuracy = training.history['accuracy']
     loss = training.history['loss']
+    # Plot accuracy vs. loss during training
     twoAxisEpochsPlot(accuracy, loss, len(loss))
 
     print("---TESTING---")
-    # test + confusion matrix
+    # test model
     predictions = ncaaw_rnn.predict(X_test, batch_size=32, use_multiprocessing=True, verbose=True)
     evaluation = ncaaw_rnn.evaluate(X_test, y_test, batch_size=32, use_multiprocessing=True, verbose=True)
 
     final_predictions = np.asarray([sequence[-1][0] for sequence in predictions])
     y_test_final = np.asarray([sequence[-1] for sequence in y_test])
 
+    # Round predictions for firm WIN/LOSS guesses
     true_accuracy = float(np.sum(np.round(final_predictions).astype(int) == y_test_final.astype(int))) / len(y_test_final)
     print("Firm Prediction Accuracy: ", true_accuracy)
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -8,12 +8,12 @@ from data_processing import team_list
 
 #---Various required sections for project website---
 def introduction_page():
-    st.header("NNostradamus Introduction")
+    st.header("Meet :rainbow[NNostradamus]!")
     st.image("cover_image.jpg", caption="NCAA Women's Basketball popularity has skyrocketed", use_column_width=True)
     with open('Intro.txt', 'r') as file:
         content = file.read()
     st.markdown(content)
-    st.image("angel-reese-hand-gesture.jpg", caption="Angel Reese Led the LSU Tigers to the 2023 NCAA Championship", use_column_width=True)
+    st.image("angel-reese-hand-gesture.jpg", caption="Angel Reese led the LSU Tigers to the 2023 NCAA Championship", use_column_width=True)
     with open('Intro2.txt', 'r') as file:
         content = file.read()
     st.markdown(content)
@@ -26,16 +26,43 @@ def live_results(newest_results, selected_team):
     st.markdown(newest_results[selected_team])
 
 def data_gathering():
-    st.header("Data Gathering")
-    st.write("Include Gathering and cleaning")
+    st.header("Data Gathering and Cleaning")
+    with open('DataPrep.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("data_lifecycle.jpg",
+             caption="The flowchart depicts how data goes from the original format to being used by the model",
+             use_column_width=True)
 
 def analysis_page():
-    st.header("Analysis")
-    st.write("Models/Methods Used")
+    st.header("Models + Methods")
+    with open('Analysis.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("lstm.png",
+             caption="A simplified version of a single timestep in an LSTM network",
+             use_column_width=True)
+    with open('Analysis2.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("model_summary.png",
+             caption="Keras model summary of LSTM",
+             use_column_width=True)
 
 def results_page():
     st.header("Technical Results")
-    st.write("This is the place for technical results")
+    with open('Results.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("model_loss_graph.jpeg",
+             caption="Loss vs. Accuracy of NNostradamus over training epochs",
+             use_column_width=True)
+    with open('Results2.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("raw_predictions.png",
+             caption="Raw outcome of the model predicting the likelihood an array of teams will win their games",
+             use_column_width=True)
 
 def about_me():
     st.header("About the Author")
@@ -53,15 +80,18 @@ def conclusion():
         content = file.read()
     st.markdown(content)
     st.image("nostradamus.jpg", caption="Nostradamus was renowned for his prophecies, and has shown a knack for basketball as well", use_column_width=True)
-
+    with open('Conclusion2.txt', 'r') as file:
+        content = file.read()
+    st.markdown(content)
+    st.image("trophy.jpg", caption="", use_column_width=True)
 run_once = False
 
 if __name__ == '__main__':
 
-    # ---Everything below here is website-related---
+    # ---This is rerun on any website event---
     home_button = st.sidebar.button("Introduction")
     schedule_button = st.sidebar.button("Scheduled Games + Predictions")
-    data_button = st.sidebar.button("Data and Prep")
+    data_button = st.sidebar.button("Data Preparation")
     analysis_button = st.sidebar.button("Analysis")
     results_button = st.sidebar.button("Technical Results")
     conclusion_button = st.sidebar.button("Conclusions")
@@ -84,7 +114,7 @@ if __name__ == '__main__':
     elif about_button:
         about_me()
     elif schedule_button:
-        ncaaw_model = load_model('ncaaw_model.h5')
+        ncaaw_model = load_model('ncaaw_model.keras')
         X_current = dp.get_current_year(use_API=False)
         st.session_state.predictions_text = dp.make_predictions(X_current, ncaaw_model)
         st.header("Live Predictions")
@@ -95,8 +125,8 @@ if __name__ == '__main__':
         st.session_state.selected_team = selected_team
         live_results(newest_results=st.session_state.predictions_text, selected_team=st.session_state.selected_team)
     elif update_button:
-        print("Update button pressed!!~!!")
-        ncaaw_model = load_model('ncaaw_model.h5')
+        print("Update button pressed!")
+        ncaaw_model = load_model('ncaaw_model.keras')
         X_current = dp.get_current_year(use_API=True)
         st.session_state.predictions_text = dp.make_predictions(X_current, ncaaw_model)
         st.header("Live Predictions")
@@ -114,4 +144,3 @@ if __name__ == '__main__':
         selected_team = st.selectbox("Team:", options)
         st.session_state.selected_team = selected_team
         live_results(newest_results=st.session_state.predictions_text, selected_team=st.session_state.selected_team)
-    # print("ST.SELECTION AT END OF MAIN:", st.session_state.selected_team)
